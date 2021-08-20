ec4e56b4c699e22d4615af5494bf0aa7c4af275e
Yoshua-chan <filipek.kacper311@gmail.com>
2021-08-20 02:07:25 +0200

    updated gitignore

--- sortowanie/header.h
+++ sortowanie/header.h
@@ -3,5 +3,6 @@
 void swap(int arr[], int x, int y);
 void bubblesort(int arr[], int size);
 void selectsort(int arr[], int size);
+void insertionsort(int arr[], int size);
 void quicksort(int arr[], int left, int right);
 void mergesort(int arr[], int left, int right);
